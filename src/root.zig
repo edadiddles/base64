351a2d55f52c7d1ddd28c77ee8794d6b179061ce
Edward Ruzek <edadiddles@hotmail.com>
2025-11-15 09:41:52 -0500

    adding logic to calculate the encoded length and only allocate the amount of memory needed

--- src/root.zig
+++ src/root.zig
@@ -13,18 +13,14 @@ const Base64 = struct{
     pub fn get_char(self: Base64, idx: usize) u8 {
         return self.lkup_tbl[idx];
     }
+
+    fn encode_length(_: Base64, input_len: usize) usize {
+        return ((input_len/4)+1)*4;
+    }
     
-    pub fn encode(self: Base64, str: []const u8) ![]u8 {
-        
-        const allocator = std.testing.allocator;
-        const buffer = try allocator.alloc(u8, 1024);
-        defer allocator.free(buffer);
+    pub fn encode(self: Base64, allocator: std.mem.Allocator, str: []const u8) ![]u8 { 
+        const buffer = try allocator.alloc(u8, self.encode_length(str.len));
 
-        for (str) |c| {
-            std.debug.print("{c}", .{c});
-        }
-
-        std.debug.print("\n", .{});
         var window_buf = [3]u8{0, 0, 0};
         var window_buf_idx: u8 = 0;
         var buffer_idx: u8 = 0;
@@ -33,36 +29,32 @@ const Base64 = struct{
             window_buf_idx += 1;
             
             if (window_buf_idx == 3) {
-                buffer[buffer_idx] = self.lkup_tbl[window_buf[0] >> 2];
-                buffer[buffer_idx+1] = self.lkup_tbl[((window_buf[0] & 0x03) << 4) + (window_buf[1] >> 4)];
-                buffer[buffer_idx+2] = self.lkup_tbl[((window_buf[1] & 0x0f) << 2) + (window_buf[2] >> 6)];
-                buffer[buffer_idx+3] = self.lkup_tbl[window_buf[2] & 0x0f];
+                buffer[buffer_idx] = self.get_char(window_buf[0] >> 2);
+                buffer[buffer_idx+1] = self.get_char(((window_buf[0] & 0x03) << 4) + (window_buf[1] >> 4));
+                buffer[buffer_idx+2] = self.get_char(((window_buf[1] & 0x0f) << 2) + (window_buf[2] >> 6));
+                buffer[buffer_idx+3] = self.get_char(window_buf[2] & 0x0f);
                 window_buf_idx = 0;
                 buffer_idx += 4;
             }
         }
 
         if (window_buf_idx == 1) {
-            buffer[buffer_idx] = self.lkup_tbl[window_buf[0] >> 2];
-            buffer[buffer_idx+1] = self.lkup_tbl[((window_buf[0] & 0x03) << 4)];
+            buffer[buffer_idx] = self.get_char(window_buf[0] >> 2);
+            buffer[buffer_idx+1] = self.get_char(((window_buf[0] & 0x03) << 4));
             buffer[buffer_idx+2] = '=';
             buffer[buffer_idx+3] = '=';
             buffer_idx += 4;
         }
 
         if (window_buf_idx == 2) {
-            buffer[buffer_idx] = self.lkup_tbl[window_buf[0] >> 2];
-            buffer[buffer_idx+1] = self.lkup_tbl[((window_buf[0] & 0x03) << 4) + (window_buf[1] >> 4)];
-            buffer[buffer_idx+2] = self.lkup_tbl[((window_buf[1] & 0x0f) << 2)];
+            buffer[buffer_idx] = self.get_char(window_buf[0] >> 2);
+            buffer[buffer_idx+1] = self.get_char(((window_buf[0] & 0x03) << 4) + (window_buf[1] >> 4));
+            buffer[buffer_idx+2] = self.get_char(((window_buf[1] & 0x0f) << 2));
             buffer[buffer_idx+3] = '=';
             buffer_idx += 4;
         }
 
-        for (buffer) |b| {
-            std.debug.print("{c}", .{b});
-        }
-        std.debug.print("\n", .{});
-        return &[0]u8{};
+        return buffer;
     }
 
     pub fn decode(self: Base64, str: []u8) []u8 {
@@ -76,21 +68,29 @@ const Base64 = struct{
 
 test "encode Hi" {
     const b64 = Base64.init();
-    _ = try b64.encode("Hi");
+    const allocator = std.testing.allocator;
+    const actual = try b64.encode(allocator, "Hi");
+    defer allocator.free(actual);
+    std.debug.print("{s}\n", .{ actual });
+    try std.testing.expectEqualStrings(actual, "SGk=");
 }
 
 test "encode Hello World" {
-    const b64 = Base64.init();
-    _ = try b64.encode("Hello World");
+    //const b64 = Base64.init();
+    //const allocator = std.testing.allocator;
+    //const actual = try b64.encode(allocator, "Hello World");
+    //allocator.free(actual);
 }
 
 test "encode foobar" {
-    const b64 = Base64.init();
-    _ = try b64.encode("");
-    _ = try b64.encode("f");
-    _ = try b64.encode("fo");
-    _ = try b64.encode("foo");
-    _ = try b64.encode("foob");
-    _ = try b64.encode("fooba");
-    _ = try b64.encode("foobar");
+    //const b64 = Base64.init();
+    //const allocator = std.testing.allocator;
+    //_ = try b64.encode(allocator, "");
+    //_ = try b64.encode(allocator, "f");
+    //_ = try b64.encode(allocator, "fo");
+    //_ = try b64.encode(allocator, "foo");
+    //_ = try b64.encode(allocator, "foob");
+    //_ = try b64.encode(allocator, "fooba");
+    //_ = try b64.encode(allocator, "foobar");
+    //allocator.free(actual);
 }
