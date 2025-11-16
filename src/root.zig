e12144c2b0248854306dd47062d6b0588d380adb
Edward Ruzek <edadiddles@hotmail.com>
2025-11-16 07:51:05 -0500

    successfully passing foobar test example from rfc4648

--- src/root.zig
+++ src/root.zig
@@ -19,6 +19,10 @@ const Base64 = struct{
     }
     
     pub fn encode(self: Base64, allocator: std.mem.Allocator, str: []const u8) ![]u8 { 
+        if (str.len == 0) {
+            return "";
+        }
+
         const buffer = try allocator.alloc(u8, self.encode_length(str.len));
 
         var window_buf = [3]u8{0, 0, 0};
@@ -32,7 +36,7 @@ const Base64 = struct{
                 buffer[buffer_idx] = self.get_char(window_buf[0] >> 2);
                 buffer[buffer_idx+1] = self.get_char(((window_buf[0] & 0x03) << 4) + (window_buf[1] >> 4));
                 buffer[buffer_idx+2] = self.get_char(((window_buf[1] & 0x0f) << 2) + (window_buf[2] >> 6));
-                buffer[buffer_idx+3] = self.get_char(window_buf[2] & 0x0f);
+                buffer[buffer_idx+3] = self.get_char(window_buf[2] & 0x3f);
                 window_buf_idx = 0;
                 buffer_idx += 4;
             }
@@ -69,10 +73,10 @@ const Base64 = struct{
 test "encode Hi" {
     const b64 = Base64.init();
     const allocator = std.testing.allocator;
+    
     const actual = try b64.encode(allocator, "Hi");
     defer allocator.free(actual);
-    std.debug.print("{s}\n", .{ actual });
-    try std.testing.expectEqualStrings(actual, "SGk=");
+    try std.testing.expectEqualStrings("SGk=", actual);
 }
 
 test "encode Hello World" {
@@ -83,14 +87,34 @@ test "encode Hello World" {
 }
 
 test "encode foobar" {
-    //const b64 = Base64.init();
-    //const allocator = std.testing.allocator;
-    //_ = try b64.encode(allocator, "");
-    //_ = try b64.encode(allocator, "f");
-    //_ = try b64.encode(allocator, "fo");
-    //_ = try b64.encode(allocator, "foo");
-    //_ = try b64.encode(allocator, "foob");
-    //_ = try b64.encode(allocator, "fooba");
-    //_ = try b64.encode(allocator, "foobar");
-    //allocator.free(actual);
+    const b64 = Base64.init();
+    const allocator = std.testing.allocator;
+    
+    const actual0 = try b64.encode(allocator, ""); 
+    defer allocator.free(actual0);
+    try std.testing.expectEqualStrings("", actual0);
+    
+    const actual1 = try b64.encode(allocator, "f");
+    defer allocator.free(actual1);
+    try std.testing.expectEqualStrings("Zg==", actual1);
+
+    const actual2 = try b64.encode(allocator, "fo");
+    defer allocator.free(actual2);
+    try std.testing.expectEqualStrings("Zm8=", actual2);
+    
+    const actual3 = try b64.encode(allocator, "foo");
+    defer allocator.free(actual3);
+    try std.testing.expectEqualStrings("Zm9v", actual3);
+   
+    const actual4 = try b64.encode(allocator, "foob");
+    defer allocator.free(actual4);
+    try std.testing.expectEqualStrings("Zm9vYg==", actual4);
+    
+    const actual5 = try b64.encode(allocator, "fooba");
+    defer allocator.free(actual5);
+    try std.testing.expectEqualStrings("Zm9vYmE=", actual5);
+    
+    const actual6 = try b64.encode(allocator, "foobar");
+    defer allocator.free(actual6);
+    try std.testing.expectEqualStrings("Zm9vYmFy", actual6);
 }
